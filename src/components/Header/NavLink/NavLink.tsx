a7d9119667e37bd9878ab55cbee35197b30e46a8
OJ Abba <phavorabba@gmail.com>
2024-11-24 03:46:36 +0000

    Create NavLink component

---
+++ src/components/Header/NavLink/NavLink.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import classNames from 'classnames'
+import Link from 'next/link'
+import { ReactNode } from 'react'
+import styles from './NavLink.module.css'
+import { usePathname } from 'next/navigation'
+
+interface NavLinkProps extends React.ComponentPropsWithRef<'a'> {
+  className?: string
+  children: ReactNode
+}
+export default function NavLink(props: Readonly<NavLinkProps>) {
+  const { href = '', children, className, ...rest } = props
+
+  const pathname = usePathname()
+  console.log(pathname)
+
+  const linkIsActive = pathname.startsWith(href)
+  return (
+    <Link
+      href={href}
+      className={classNames(className, styles.navLink, {
+        [styles.activeLink]: linkIsActive,
+      })}
+      {...rest}
+    >
+      {children}
+    </Link>
+  )
+}
